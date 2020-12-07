6dc29d9bd48ab121972c185bc6fc51a2bbf9cc59
Preston Lamb <preston.j.lamb@gmail.com>
2020-12-07 14:39:38 -0700

    feat: 🎸 added the functionality to the service
    Added the actual functionality to the service, as well as needed
    interfaces and classes; setup the APP_INITIALIZER token functionality to
    run this when the app is bootstrapping
    
    Closes: #1

--- libs/angular-svg-icon-preloader/src/lib/angular-svg-icon-preloader/angular-svg-icon-preloader.service.ts
+++ libs/angular-svg-icon-preloader/src/lib/angular-svg-icon-preloader/angular-svg-icon-preloader.service.ts
@@ -1,9 +1,60 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, Optional } from '@angular/core';
+import { SvgIconRegistryService } from 'angular-svg-icon';
+import { Subject } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { AngularSvgIconPreloaderConfig } from '../angular-svg-icon-preloader-config.class';
+import { CustomIconData, IconImageFile } from '../icon.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AngularSvgIconPreloaderService {
+  private configUrl: string = './assets/icons.json';
+  private iconsFileData: {
+    iconImageFiles: IconImageFile[];
+    customIcons: CustomIconData[];
+  };
+  public configSubject: Subject<any> = new Subject<any>();
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient,
+    @Optional() config: AngularSvgIconPreloaderConfig,
+    private _iconRegistry: SvgIconRegistryService
+  ) {
+    if (config && config.configUrl) {
+      this.configUrl = config.configUrl;
+    }
+  }
+
+  loadConfig(): Promise<any> {
+    return this._http
+      .get(this.configUrl)
+      .toPromise()
+      .then(
+        (configData: {
+          iconImageFiles: IconImageFile[];
+          customIcons: CustomIconData[];
+        }) => {
+          this.iconsFileData = configData;
+          this.loadIcons();
+        }
+      )
+      .catch((err: any) => {
+        this.iconsFileData = { customIcons: [], iconImageFiles: [] };
+        this.loadIcons();
+      });
+  }
+
+  loadIcons() {
+    this.iconsFileData.iconImageFiles.forEach((i: IconImageFile) => {
+      this._iconRegistry
+        .loadSvg(i.iconPath, i.iconName)
+        .pipe(take(1))
+        .subscribe();
+    });
+    this.iconsFileData.customIcons.forEach((i: CustomIconData) => {
+      this._iconRegistry.addSvg(i.iconName, i.iconData);
+    });
+  }
 }
