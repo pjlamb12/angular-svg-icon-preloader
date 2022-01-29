ffea6a95fbfb25acdf8597c15f16cc2096f0415e
Preston Lamb <preston.j.lamb@gmail.com>
2022-01-29 01:19:02 -0700

    chore: update Angular and Nx versions
    Breaking Change

--- libs/angular-svg-icon-preloader/jest.config.js
+++ libs/angular-svg-icon-preloader/jest.config.js
@@ -9,6 +9,10 @@ module.exports = {
 		},
 	},
 	coverageDirectory: '../../coverage/libs/angular-svg-icon-preloader',
+	transform: {
+		'^.+.(ts|mjs|js|html)$': 'jest-preset-angular',
+	},
+	transformIgnorePatterns: ['node_modules/(?!.*.mjs$)'],
 	snapshotSerializers: [
 		'jest-preset-angular/build/serializers/no-ng-attributes',
 		'jest-preset-angular/build/serializers/ng-snapshot',
