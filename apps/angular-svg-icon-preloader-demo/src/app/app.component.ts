307fdb9445e36f5bc4e6956c5b4e6cead2c9c2a1
Preston Lamb <preston.j.lamb@gmail.com>
2025-01-29 23:28:04 -0700

    chore: update workspace to latest
    relates to #22

--- apps/angular-svg-icon-preloader-demo/src/app/app.component.ts
+++ apps/angular-svg-icon-preloader-demo/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'angular-svg-icon-preloader-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'angular-svg-icon-preloader-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
 })
 export class AppComponent {
   title = 'angular-svg-icon-preloader-demo';
