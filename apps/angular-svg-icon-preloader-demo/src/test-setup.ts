4d3d6f0fd23f4ee16a0157a9d40804cc5b6588eb
Preston Lamb <preston.j.lamb@gmail.com>
2026-02-06 21:28:55 -0700

    chore: update to nx 22

--- apps/angular-svg-icon-preloader-demo/src/test-setup.ts
+++ apps/angular-svg-icon-preloader-demo/src/test-setup.ts
@@ -6,5 +6,6 @@ globalThis.ngJest = {
 		errorOnUnknownProperties: true,
 	},
 };
+import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
 
-import 'jest-preset-angular/setup-jest';
+setupZoneTestEnv();
